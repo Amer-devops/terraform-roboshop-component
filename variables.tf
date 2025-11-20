eb692d7d9369cb9194c5bcd14101366a9dfd0e88
amer-devops <amerahmed.official@gmail.com>
2025-11-20 18:35:06 +0530

    terraform

---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "project_name" {
+    default = "roboshop"  
+}
+
+variable "environment" {
+    default = "dev"  
+}
+variable "domain_name" {
+    default = "daws86s.icu"
+}
+variable "component" {
+  default = "catalogue"
+}
+variable "rule_priority" {
+    default = 10
+  
+}
